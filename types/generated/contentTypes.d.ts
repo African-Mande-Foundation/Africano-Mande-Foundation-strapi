e39f4fd189e08cd98aeab061264fd9f338f2156c
melvin-mango <melvinlewisusiu@gmail.com>
2025-10-06 18:03:54 +0300

    Image Integration

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -691,7 +691,7 @@ export interface ApiImageGalleryImageGallery
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     description: Schema.Attribute.String;
-    Images: Schema.Attribute.Media<'files' | 'videos', true> &
+    Images: Schema.Attribute.Media<'files' | 'images', true> &
       Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
@@ -700,6 +700,7 @@ export interface ApiImageGalleryImageGallery
     > &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
+    title: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
