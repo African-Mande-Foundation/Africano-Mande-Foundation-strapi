6219559991b0bffb51cbe5a277d8fdefed3ace0d
melvin-mango <melvinlewisusiu@gmail.com>
2025-10-21 01:25:24 +0300

    added user photoUrl

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1518,6 +1518,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    photoUrl: Schema.Attribute.Text;
     provider: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     resetPasswordToken: Schema.Attribute.String & Schema.Attribute.Private;
